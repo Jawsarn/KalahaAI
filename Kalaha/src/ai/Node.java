16849e402fb6f770802c86625af086c569967a0d
slowey <lucas.holmqvist@gmail.com>
2016-09-15 14:55:51 +0200

    Added start of Node

--- Kalaha/src/ai/Node.java
+++ Kalaha/src/ai/Node.java
@@ -1,7 +1,21 @@
 package ai;
+import kalaha.*;
+
 
 /**
  * Created by Becca on 2016-09-15.
  */
-public class Node {
+public class Node{
+    int m_depth;
+    int m_maxDepth;
+    GameState m_gameState;
+    Node[] childNodes = new Node[6];
+    int m_bestValue = 0;
+    int m_bestNodeIndex = -1;
+    Node(int p_depth, GameState p_gameState){
+        m_depth = p_depth;
+        m_gameState = p_gameState;
+    }
+    
+
 }
